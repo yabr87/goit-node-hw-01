129ad9e25a91528fce490134aa10210ac439cb87
yabr87 <108272773+yabr87@users.noreply.github.com>
2023-03-24 17:28:40 +0200

    added commands and invokeAction

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const contacts = require('./contacts');
+
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+
+    case 'get':
+      const getContact = await contacts.getContactById(id);
+      return console.table(getContact);
+
+    case 'add':
+      const nweContact = await contacts.addContact(name, email, phone);
+      return console.table(nweContact);
+
+    case 'remove':
+      const deleteContact = await contacts.removeContact(id);
+      return console.table(deleteContact);
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+};
+
+invokeAction(argv);
